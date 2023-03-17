067790ff4a27f00398b8813984bd88cfe2b96ab8
Aditya Anand Thakur <anandaditya444@gmail.com>
2023-03-17 21:28:13 +0530

    Update WordBreakingProblem.cpp

--- Backtracking Day9+10/WordBreakingProblem.cpp
+++ Backtracking Day9+10/WordBreakingProblem.cpp
@@ -37,6 +37,42 @@ public:
 
 
 
+
+/*
+
+    Time Complexity : O(N^3), There are two nested loops, and substring computation at each iteration. Overall
+    that results in O(N^3) time complexity.
+
+    Space Complexity : O(N), Length of dp array is N+1.
+    
+    Solved using String + DP(Tabulation) + Hash Table.
+
+*/
+
+
+/***************************************** Approach 3 *****************************************/
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, 0);
+        dp[0] = true;
+        unordered_set<string> set(wordDict.begin(), wordDict.end());
+        for(int i=1; i<=s.size(); i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && set.count(s.substr(j, i-j))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
+
+
+
+
 // We have a set of words in a dictionary.
 
 void wordBreakUtil(string s, int n, string result)
